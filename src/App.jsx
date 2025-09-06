47b97a5532d1b221636074373531e2bf422e75ba
phquartin <phquartinmatos@sempreceub.com>
2025-09-06 20:34:53 -0300

    feat: criado um componente de listagem de tasks e adicionado functions para completar e excluir tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,54 @@
-import {useState} from "react";
 import Tasks from "./components/Tasks.jsx";
 import AddTask from "./components/AddTask.jsx";
+import {useState} from "react";
 
 function App() {
+    const [tasks, setTasks] = useState([
+        {
+        id: 1,
+        title: "Estudar React",
+        description: "Integrar o react para o projeto Parada Do Frango",
+        isCompleted: false
+        },
+        {
+            id: 2,
+            title: "Estudar Tailwind",
+            description: "Integrar o Tailwind para o projeto Parada Do Frango",
+            isCompleted: false
+        },
+        {
+            id: 3,
+            title: "Estudar Pagination",
+            description: "Integrar o Pagination para o projeto Parada Do Frango",
+            isCompleted: false
+        }
+    ])
+
+    function onDeleteTaskClick(id) {
+        const newTasks = tasks.filter(t => t.id !== id)
+        setTasks(newTasks);
+    }
+    function onCompleteTaskClick(id) {
+        const newTasks = tasks.map(t => {
+            // ATUALIZAR O ESTADO
+            if(t.id === id) {
+                return {...t, isCompleted: !t.isCompleted}
+            }
+            return t;
+        })
+        setTasks(newTasks);
+    }
+
     return(
-        <div>
-            <h1>Gerenciador de Tarefas</h1>
-            <AddTask />
-            <Tasks />
+        <div className={"w-screen h-screen bg-slate-500 flex justify-center p-6"}>
+            <div className="w-[500px]">
+                <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de Tarefas</h1>
+                <AddTask />
+                <Tasks tasks={tasks}
+                       onCompleteTaskClick={onCompleteTaskClick}
+                       onDeleteTaskClick={onDeleteTaskClick}
+                />
+            </div>
         </div>
     );
 }
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,8 +1,28 @@
-function Tasks() {
+import {ChevronRightIcon, TrashIcon} from "lucide-react";
+
+function Tasks({tasks, onCompleteTaskClick, onDeleteTaskClick}) {
     return(
-        <h1>
-            Tarefas
-        </h1>
+            <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+                {tasks.map(t =>
+                <li key={t.id} className="flex gap-2">
+                    <button
+                        onClick={() => onCompleteTaskClick(t.id)}
+                        className={`bg-slate-400 w-full text-left text-white p-2 rounded-md 
+                        ${t.isCompleted && 'line-through'}
+                        `}>
+                        {t.title}
+                    </button>
+                    <button className="bg-slate-400 p-2 rounded-md text-white">
+                        <ChevronRightIcon />
+                    </button>
+                    <button
+                        onClick={() => onDeleteTaskClick(t.id)}
+                        className="bg-slate-400 p-2 rounded-md text-white">
+                        <TrashIcon />
+                    </button>
+                </li>
+            )}
+            </ul>
     )
 }
 export default Tasks;
