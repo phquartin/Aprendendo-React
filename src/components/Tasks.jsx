fde3659bbbf134c4bb915bbd0bdd338c8fe6e162
phquartin <phquartinmatos@sempreceub.com>
2025-09-07 17:12:24 -0300

    feat: Criado a Page para ver detalhes

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,16 @@
 import {ChevronRightIcon, TrashIcon} from "lucide-react";
+import {redirect, useNavigate} from "react-router-dom";
 
 function Tasks({tasks, onCompleteTaskClick, onDeleteTaskClick}) {
+    const navigate = useNavigate();
+
+    function onSeeDetailsClick(t) {
+        const query = new URLSearchParams()
+        query.set("title", t.title)
+        query.set("description", t.description)
+        navigate(`/task?${query.toString()}`)
+    }
+
     return(
             <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
                 {tasks.map(t =>
@@ -12,7 +22,9 @@ function Tasks({tasks, onCompleteTaskClick, onDeleteTaskClick}) {
                         `}>
                         {t.title}
                     </button>
-                    <button className="bg-slate-400 p-2 rounded-md text-white">
+                    <button className="bg-slate-400 p-2 rounded-md text-white"
+                            onClick={() => onSeeDetailsClick(t)}
+                    >
                         <ChevronRightIcon />
                     </button>
                     <button
---
+++ src/pages/TaskPage.jsx
@@ -0,0 +1,31 @@
+import {useNavigate, useSearchParams} from "react-router-dom";
+import {ChevronLeftIcon} from "lucide-react";
+
+function TaskPage() {
+    const navigate = useNavigate();
+    const [searchParams] = useSearchParams();
+    const title = searchParams.get("title");
+    const description = searchParams.get("description");
+    return(
+        <div className={"w-screen h-screen bg-slate-500 flex justify-center p-6"}>
+            <div className="w-[500px] space-y-4">
+
+                <div className="flex justify-center relative">
+                    <button className="absolute left-0 top-0 bottom-0 text-white"
+                            onClick={() => navigate(-1)}>
+                            <ChevronLeftIcon />
+                    </button>
+                    <h1 className="text-3xl text-slate-100 font-bold text-center">Detalhes da tarefa</h1>
+                </div>
+
+                <div className="space-y-4 p-4 bg-slate-200 rounded-md shadow">
+                    <h2 className="text-xl text-slate-600 font-bold">{title}</h2>
+                    <p className="text-slate-500">{description}</p>
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+export default TaskPage;
