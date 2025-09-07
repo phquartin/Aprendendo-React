ca73abb209013d8b22e9f040bd1aab46eecd5205
phquartin <phquartinmatos@sempreceub.com>
2025-09-06 21:09:47 -0300

    feat: criado uma function para adicionar tasks e criado um componente para usar essa function

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,8 +1,35 @@
-function AddTask() {
+import {useState} from "react";
+
+function AddTask({onAddTaskSubmit}) {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
     return(
-        <h1>
-            Adicionar Tarefa
-        </h1>
+        <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+            <input type="text" placeholder="Digite o titulo da tarefa"
+                   className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+                   value={title}
+                   onChange={(e) => setTitle(e.target.value)}
+            />
+            <input type="text" placeholder="Digite a descricao da tarefa"
+                   className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+                   value={description}
+                   onChange={(e) => setDescription(e.target.value)}
+            />
+            <button
+                onClick={() => {
+                    if (title.length <= 5 || description.length <= 5) {
+                         return alert("Preencha todos os campos com no minimo 5 caracteres");
+                    }
+                    onAddTaskSubmit(title, description)
+                    setTitle("")
+                    setDescription("")
+                }}
+                disabled={title.length === 0 || description.length === 0}
+                className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium"
+            >
+                Adicionar Tarefa
+            </button>
+        </div>
     )
 }
 
